02e3a31afad8620c1cc9708c0e2081c2991fdb86
salma-h-wagdy <salmahisham139@gmail.com>
2024-08-05 11:29:52 +0300

    added test suite

---
+++ src/test/java/com/testing/AirlineSystemTestSuite.java
@@ -0,0 +1,15 @@
+package com.testing;
+
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        BookingTests.class,
+        AdminTest.class,
+        ClientsTripsTest.class, // Replace with actual class name
+        FlightsTest.class   // Replace with actual class name
+})
+public class AirlineSystemTestSuite {
+}
